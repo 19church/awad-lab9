e77c8972545e70a59f71b5233ca8634cf51607b6
19church <saint2737@gmail.com>
2023-09-26 23:40:45 +0700

    first commit

---
+++ 8-2.js
@@ -0,0 +1,22 @@
+const expressFunction = require('express');
+const expressApp = expressFunction();
+
+const students = [
+    {stdid: 'B6304249', name: 'Thanakrit Namnual'},
+    {stdid: 'B5222299', name: 'Jaidee Deejai'},
+];
+
+expressApp.get('/api/resource/students/:stdid', function(req, res) {
+    const stdid = req.params.stdid;
+    if (stdid == 'B6304249') {
+        res.status(200).send(students[0]);
+    } else if (stdid == 'B5222299') {
+        res.status(200).send(students[1]);
+    } else {
+        res.status(404).send('Error 404 not found');
+    }
+});
+
+expressApp.listen(3000, function() {
+    console.log('Listening on port 3000');
+});
